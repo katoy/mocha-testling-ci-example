42b41ff3a8a95f30ece14a12a89de386c6789458
katoy <youichikato@gmail.com>
2013-03-04 16:47:11 +0900

    add.js -> app.coffee

---
+++ src/app.coffee
@@ -0,0 +1,4 @@
+# this is the module we're testing
+add = (n) ->
+  n * n
+module.exports.add = add
--- src/app.js
+++
@@ -1,7 +0,0 @@
-// this is the module we're testing
-
-function add(n) {
-    return n * n;
-};
-
-module.exports.add = add;
